6eadcc681e139e86a6239afdd9ba8d4df5ae788e
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2025-03-17 14:58:17 +0800

    Update project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,16 @@
 import MechHub from '../assets/images/MechHub.png';
 import FSO from '../assets/images/FSO.png';
 import ShowerBreak from '../assets/images/ShowerBreak.png'
+import Chatbot from '../assets/images/Chatbot.png'
 
 const projects = [
+  {
+    title: 'Chatbot for Task Management',
+    description: 'A chatbot for users to manage tasks via Graphical User Interface GUI. Developed CRUD operations using OOP principles for clean code.',
+    code: 'https://github.com/feliciahmq/ip',
+    stack: 'Java, JavaFX, Gradle, JUnit',
+    thumbnail: Chatbot,
+    },
   {
     title: 'Shower Reminder TeleBot',
     description: 'A fun Telegram bot for NUS Hack&Roll 2025 to encourage good hygiene by gamifying our shower routine! Features include QR code integration, ML-based photo verification for showerhead detection, a shower timer, and daily reminders. Users can track their showering habits interactively through Telegram.',
