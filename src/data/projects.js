379bd8a92e456e26c17f42fe331d19e084701ec4
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2025-03-23 22:16:33 +0800

    Update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,15 +2,23 @@ import MechHub from '../assets/images/MechHub.png';
 import FSO from '../assets/images/FSO.png';
 import ShowerBreak from '../assets/images/ShowerBreak.png'
 import Chatbot from '../assets/images/Chatbot.png'
+import JobTracker from '../assets/images/JobTracker.png'
 
 const projects = [
+	{
+    title: 'Job Tracker',
+    description: 'Track your job hunt. A CRUD app to manage job applications, built with T3 Stack. Frequent deprecations, and scattered documentations so it got slightly annoying building this app. Loved Shadcn for their clean and modern components, and type safety with tRPC and Zod.',
+    code: 'https://github.com/feliciahmq/t3-job-app',
+    stack: 'NextJS, Prisma, TailwindCSS, tRPC, Zod, Shadcn-UI',
+    thumbnail: JobTracker,
+	},
   {
     title: 'Chatbot for Task Management',
     description: 'A chatbot for users to manage tasks via Graphical User Interface GUI. Developed CRUD operations using OOP principles for clean code.',
     code: 'https://github.com/feliciahmq/ip',
     stack: 'Java, JavaFX, Gradle, JUnit',
     thumbnail: Chatbot,
-    },
+	},
   {
     title: 'Shower Reminder TeleBot',
     description: 'A fun Telegram bot for NUS Hack&Roll 2025 to encourage good hygiene by gamifying our shower routine! Features include QR code integration, ML-based photo verification for showerhead detection, a shower timer, and daily reminders. Users can track their showering habits interactively through Telegram.',
